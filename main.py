f9a7d5c432f446572ed3ad4843068910e239a718
pankajkumar23 <pankajr7508@gmail.com>
2025-12-03 18:32:06 +0530

    Setting up the automation pipeline

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+def run():
+    print("-----------------------------------------------")
+    print("--------------automation pipline is sucessfull.")
+    print("-----------------------------------------------")
+
+
+if __name__ == "__main__":
+    run()
